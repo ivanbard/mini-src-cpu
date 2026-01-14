09c575a5a0f023e4810fcccbe9288204023824af
ivanbard <ivanbardziyan@gmail.com>
2026-01-14 12:30:43 -0500

    testbench complete

--- registers_test.v
+++ registers_test.v
@@ -21,4 +21,29 @@ module tb_regfile16;
     initial clk = 0;
     always #5 clk = ~clk;
 
+    initial begin
+        reset = 1;
+        rin = 16'b0;
+        rout = 16'b0;
+        bus = 32'b0;
+
+        //hold reset for 2 rising edges to be safe
+        repeat(2) @(posedge clk);
+        reset = 0;
+
+        // WRITE TO R2
+        bus = 32'hAAAA5555; //put in the hex we want to put into r2
+        rin =16'b0000_0000_0000_0100; //enable r2 in the in
+        @(posedge clk);
+        rin = 16'b0; //stop write
+
+        // READ FROM R2
+        rout = 16'b0000_0000_0000_0100;
+        #1; //small delay to be safe for the mux
+
+        if (bus_out !== 32'hAAAA5555) $display("error: bus_out=%h", bus_out);
+        else $display("works")
+        $finish
+    end
+
 endmodule
