023962ba81bc46c0cc8cf04a92f33dbc166c024b
ivanbard <ivanbardziyan@gmail.com>
2026-01-19 15:04:56 -0500

    encoded 32-5 bus mux

--- project/bus_mux.v
+++ project/bus_mux.v
@@ -1,56 +1,53 @@
-module bus_mux (
-    input wire [15:0] rout,
-    input wire pc_out_en,
-    input wire hi_out_en,
-    input wire lo_out_en,
-    input wire z_hi_out_en,
-    input wire z_lo_out_en,
-    input wire mdr_out_en,
-    input wire inport_out_en,
-    input wire c_out_en,
+module bus_mux_enc (
+    input  wire [4:0]  bus_sel,
 
-    input wire [31:0] r0, input wire [31:0] r1, input wire [31:0] r2, input wire [31:0] r3,
-    input wire [31:0] r4, input wire [31:0] r5, input wire [31:0] r6, input wire [31:0] r7,
-    input wire [31:0] r8, input wire [31:0] r9, input wire [31:0] r10, input wire [31:0] r11,
-    input wire [31:0] r12, input wire [31:0] r13, input wire [31:0] r14, input wire [31:0] r15,
+    input  wire [31:0] r0,  input wire [31:0] r1,  input wire [31:0] r2,  input wire [31:0] r3,
+    input  wire [31:0] r4,  input wire [31:0] r5,  input wire [31:0] r6,  input wire [31:0] r7,
+    input  wire [31:0] r8,  input wire [31:0] r9,  input wire [31:0] r10, input wire [31:0] r11,
+    input  wire [31:0] r12, input wire [31:0] r13, input wire [31:0] r14, input wire [31:0] r15,
 
-    input wire [31:0] pc_val,
-    input wire [31:0] hi_val,
-    input wire [31:0] lo_val,
-    input wire [31:0] z_hi_val,
-    input wire [31:0] z_lo_val,
-    input wire [31:0] mdr_val,
-    input wire [31:0] inport_val,
-    input wire [31:0] c_val,
+    input  wire [31:0] hi_val,
+    input  wire [31:0] lo_val,
+    input  wire [31:0] zhigh_val,
+    input  wire [31:0] zlow_val,
+    input  wire [31:0] pc_val,
+    input  wire [31:0] mdr_val,
+    input  wire [31:0] inport_val,
+    input  wire [31:0] c_val,
 
-    output wire [31:0] bus
+    output reg  [31:0] BusMuxOut
 );
 
-    assign bus =
-        rout[0]  ? r0  :
-        rout[1]  ? r1  :
-        rout[2]  ? r2  :
-        rout[3]  ? r3  :
-        rout[4]  ? r4  :
-        rout[5]  ? r5  :
-        rout[6]  ? r6  :
-        rout[7]  ? r7  :
-        rout[8]  ? r8  :
-        rout[9]  ? r9  :
-        rout[10] ? r10 :
-        rout[11] ? r11 :
-        rout[12] ? r12 :
-        rout[13] ? r13 :
-        rout[14] ? r14 :
-        rout[15] ? r15 :
-        pc_out_en     ? pc_val     :
-        hi_out_en     ? hi_val     :
-        lo_out_en     ? lo_val     :
-        z_hi_out_en   ? z_hi_val   :
-        z_lo_out_en   ? z_lo_val   :
-        mdr_out_en    ? mdr_val    :
-        inport_out_en ? inport_val :
-        c_out_en      ? c_val      :
-        32'b0;
+    always @(*) begin
+        case (bus_sel)
+        5'd0:  BusMuxOut = r0;
+        5'd1:  BusMuxOut = r1;
+        5'd2:  BusMuxOut = r2;
+        5'd3:  BusMuxOut = r3;
+        5'd4:  BusMuxOut = r4;
+        5'd5:  BusMuxOut = r5;
+        5'd6:  BusMuxOut = r6;
+        5'd7:  BusMuxOut = r7;
+        5'd8:  BusMuxOut = r8;
+        5'd9:  BusMuxOut = r9;
+        5'd10:  BusMuxOut = r10;
+        5'd11:  BusMuxOut = r11;
+        5'd12:  BusMuxOut = r12;
+        5'd13:  BusMuxOut = r13;
+        5'd14:  BusMuxOut = r14;
+        5'd15:  BusMuxOut = r15;
+
+        5'd16: BusMuxOut = hi_val;
+        5'd17: BusMuxOut = lo_val;
+        5'd18: BusMuxOut = zhigh_val;
+        5'd19: BusMuxOut = zlow_val;
+        5'd20: BusMuxOut = pc_val;
+        5'd21: BusMuxOut = mdr_val;
+        5'd22: BusMuxOut = inport_val;
+        5'd23: BusMuxOut = c_val;
+
+        default: BusMuxOut = 32'b0;
+        endcase
+    end
 
 endmodule
