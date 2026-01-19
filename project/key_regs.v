e665ccf24d42c72d284d7eacda64f7a952825941
ivanbard <ivanbardziyan@gmail.com>
2026-01-19 11:56:12 -0500

    wip PC

--- project/key_regs.v
+++ project/key_regs.v
@@ -1,5 +1,19 @@
-module PC_reg;
+module PC_reg(
+    input wire clk, 
+    input wire reset,
+    input wire pc_in,
+    input wire [31:0] bus,
+    output reg [31:0] pc_out
+);
 
+
+    reg32 PC (
+        .clk(clk),
+        .reset(reset),
+        .en(pc_in),
+        .d_in(bus),
+        .d_out(pc_out)
+    )
 
 
 endmodule
